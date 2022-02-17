8f8d79a00323df430b2ffb03c32d01056a87f315
matthew-pryor <pryor.matthew.a@gmail.com>
2022-02-17 14:47:48 -0600

    updated url pathway in the heroes_and_villains_project urls so that the http methods funtion correctly

--- heroes_and_villains/views.py
+++ heroes_and_villains/views.py
@@ -1,5 +1,3 @@
-from itertools import product
-from signal import raise_signal
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
