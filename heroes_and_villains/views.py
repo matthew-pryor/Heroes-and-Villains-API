9a510e4970d5de9b5ffde69d0902df27cb0f6371
matthew-pryor <pryor.matthew.a@gmail.com>
2022-02-17 13:43:47 -0600

    Created get function for selecting all supers from database + created post function for creating a super

--- heroes_and_villains/views.py
+++ heroes_and_villains/views.py
@@ -7,3 +7,18 @@ from .models import Supers
 
 # Create your views here.
 
+@api_view(['GET', 'POST'])
+def heroes_and_villains_list(request):
+    
+    if request.method == 'GET':
+
+        supers = Supers.objects.all()
+        serializer = SupersSerializer(supers, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
