7bc69e632baa50dfe76182f7ef7d481ba54e4f5b
matthew-pryor <pryor.matthew.a@gmail.com>
2022-02-18 16:11:59 -0600

    set GET request query parameters for retrieving FK data

--- heroes_and_villains/views.py
+++ heroes_and_villains/views.py
@@ -13,7 +13,17 @@ def heroes_and_villains_list(request):
     
     if request.method == 'GET':
 
+        super_type_param = request.query_params.get('type')
+        sort_param = request.query_params.get('sort')
+
         supers = Super.objects.all()
+
+        if super_type_param:
+            supers = supers.filter(super_type__type=super_type_param)
+
+        if sort_param:
+            supers = supers.order_by(sort_param)
+
         serializer = SuperSerializer(supers, many=True)
         return Response(serializer.data)
 
--- type_heroes_or_villains/models.py
+++ type_heroes_or_villains/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Super_type(models.Model):
 
     type = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.type
