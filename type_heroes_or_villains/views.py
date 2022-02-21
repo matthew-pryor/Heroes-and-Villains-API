6b4bfb248ef7457a7588343db7d73b34a9da3b7d
matthew-pryor <pryor.matthew.a@gmail.com>
2022-02-21 11:43:30 -0600

    created POST functionality to SuperTypes views

--- type_heroes_or_villains/views.py
+++ type_heroes_or_villains/views.py
@@ -7,7 +7,7 @@ from .models import SuperType
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def type_heroes_or_villains_list(request):
 
     if request.method == 'GET':
@@ -15,6 +15,13 @@ def type_heroes_or_villains_list(request):
         super_types = SuperType.objects
         serializer = SuperTypeSerializer(super_types, many=True)
         return Response(serializer.data)
+
+    elif request.method == 'POST':
+
+        serializer = SuperTypeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def type_heroes_or_villains_detail(request, pk):
